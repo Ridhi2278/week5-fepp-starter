1145f3117d2fd299527b05b7bd77872d07e33c6b
Prabhleen kaur <prabhleenkaur9946@gmail.com>
2026-09-16 16:08:38 +0300

    msg

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -16,6 +16,7 @@ const Home = () => {
         const data = await response.json();
         setJobs(data);
       } catch (error) {
+
         console.error("Error fetching jobs:", error);
       }
     };
